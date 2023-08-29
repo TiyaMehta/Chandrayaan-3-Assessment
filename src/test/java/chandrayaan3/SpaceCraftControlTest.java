7fbc17cf437fec3902611eadbd013c8bc7976020
Tiya Mehta <tiyakev@gmail.com>
2023-08-29 23:48:22 +0530

    test code commit 2

--- src/test/java/chandrayaan3/SpaceCraftControlTest.java
+++ src/test/java/chandrayaan3/SpaceCraftControlTest.java
@@ -3,13 +3,13 @@ package chandrayaan3;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+//import static org.junit.jupiter.api.Assertions.assertNotNull;
+//import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Order;
+//import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("Chandrayaan-3")
